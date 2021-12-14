8f710044874394c8247616c4d443b79814ee5e82
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-14 14:02:10 +0530

    new adding LCP

--- LineComparison.java
+++ LineComparison.java
@@ -25,12 +25,71 @@ public class LineComparison {
 		System.out.println("The length of (line 1) is : " + len);
 	}
 
+	/*
+	 * UC2 Line Comparison
+	 */
+	public static void FindEqualLines() {
+		LineLength();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x3 and y3 Co-ordinates:");
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
+
+		System.out.println("Enter x4 and y4 Co-ordinates:");
+		int x4 = sc.nextInt();
+		int y4 = sc.nextInt();
+
+		len1 = (int) Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+		System.out.println("The length of (line 2) is : " + len1);
+//        L2 = len1;
+
+		if (len.equals(len1)) {
+			System.out.println("\nLines are equal");
+		} else
+			System.out.println("\nLines are not equals");
+
+	}
+
+	/*
+	 * UC3 Line Comparison
+	 */
+	public static void FindCompreLine() {
+		FindEqualLines();
+
+		if (len > len1) {
+			System.out.println("\nLine1 is Greater line2");
+		} else if (len < len1) {
+			System.out.println("\nLine1 lesser than line2");
+		}
+	}
+
+	/*
+	 * UC4 Line Comparison
+	 */
+	public static void FindCompareToLines() {
+		FindEqualLines();
+		int a = len.compareTo(len1); // 0 -1
+
+		if (a == 0) {
+			
+		} else if (a == -1) {
+
+			System.out.println("\nLine1 lesser than line2");
+		}else {
+			System.out.println("Line1 is Greater line2");
+		}
+
+	}
+
 	public static void main(String args[]) {
 		/*
 		 * calling Functions
 		 */
-		 LineLength();
-		
+		// LineLength();
+		// FindEqualLines();
+		// FindCompreLine();
+		FindCompareToLines();
 	}
 
 }
